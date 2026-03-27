6da18a3a6a8f24a239c13ad24738bbd230d52281
mamun2519 <juborajislam46@gmail.com>
2026-03-27 19:49:30 +0600

    feat: added to variable concept

---
+++ variable.py
@@ -0,0 +1,12 @@
+# declare variable
+
+name = "Mohammad Mamun"
+age = 25
+height = 5.8
+isMarried = False
+
+# print variable
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Married:", isMarried)

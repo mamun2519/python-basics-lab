4af7cede32ae5fb10aa739d3614b577c816d9365
mamun2519 <juborajislam46@gmail.com>
2026-03-27 22:09:33 +0600

    feat: added to data type concept

--- stringOperation.py
+++ stringOperation.py
@@ -10,3 +10,14 @@ print(programming[:5]) # Output: Hello
 print(programming.upper())
 # modify to lower case
 print(programming.lower())
+
+# concatination string
+first_name = "Mohammad"
+last_name = "Mamun"
+full_name = first_name + " " + last_name
+print(full_name) # Output: Mohammad Mamun
+
+# string count
+text = "Hello world, welcome to the world of programming"
+count = text.count("w")
+print("Count of 'world':", count) # Output: Count of 'world':

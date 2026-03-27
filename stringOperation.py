c312aed15331a8888c9fb44c15ad0702f3369a38
mamun2519 <juborajislam46@gmail.com>
2026-03-27 22:06:49 +0600

    feat: added to data type concept

--- stringOperation.py
+++ stringOperation.py
@@ -5,3 +5,8 @@ print(programming[2:9]) # Output: Hello world
 
 # sline from the start
 print(programming[:5]) # Output: Hello
+
+# modify to upper case
+print(programming.upper())
+# modify to lower case
+print(programming.lower())

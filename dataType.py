25c6d408c5eaedad55d006ced6f5c3978df470ea
mamun2519 <juborajislam46@gmail.com>
2026-03-27 21:46:35 +0600

    feat: added to data type concept

--- dataType.py
+++ dataType.py
@@ -8,3 +8,5 @@ is_valid = True # boolean datatype
 
 studentList = [ "John", "Alice", "Bob"] # list datatype
 studentTuple = ("mamun", "sara", "jason") # tuple datatype
+studentSet = {"michael", "sara", "jason"} # set datatype
+studentDict = {"name": "mamun", "age": 25, "height": 5.8} # dictionary datatype

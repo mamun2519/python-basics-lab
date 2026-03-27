ee9f3f12171113682a5dde05517a4324f9625b61
mamun2519 <juborajislam46@gmail.com>
2026-03-27 21:46:09 +0600

    feat: added to data type concept

--- dataType.py
+++ dataType.py
@@ -4,3 +4,7 @@ x = "Hello world" # string datatype
 y = 10 # integer datatype
 z = 3.14 # float datatype
 is_valid = True # boolean datatype
+
+
+studentList = [ "John", "Alice", "Bob"] # list datatype
+studentTuple = ("mamun", "sara", "jason") # tuple datatype

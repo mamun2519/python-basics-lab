03bd03647682100c280ff68599abde66b3907b06
mamun2519 <juborajislam46@gmail.com>
2026-03-29 22:02:52 +0600

    feat: added to tuple related concept

--- tuples.py
+++ tuples.py
@@ -1,3 +1,11 @@
 
 ## syntax the tuple
 myTuple = (1, 2, 3, 4, 5 , 6)
+
+# accessing the tuple
+print(myTuple[0])  # Output: 1
+print(len(myTuple))  # Output: 6
+
+# loop the tuple
+for i in myTuple:
+    print(i)

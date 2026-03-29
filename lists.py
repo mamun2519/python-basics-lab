ed226f6fce3729cf06a24fb2db69fcc016316740
mamun2519 <juborajislam46@gmail.com>
2026-03-29 21:21:31 +0600

    feat: added to list related concept

--- lists.py
+++ lists.py
@@ -1,3 +1,12 @@
 
 # syntax the list
-myList = [1, 2, 3, 4, 5]
+myList = [1, 2, 3, 4, 5 , 6]
+
+# accessing the list
+print(myList[0])  # Output: 1
+print(myList[2:6])  # Output: 3
+print(myList[:3])  # Output: 6
+
+# change the value
+myList[0] = 10
+print(myList)  # Output: [10, 2, 3, 4
